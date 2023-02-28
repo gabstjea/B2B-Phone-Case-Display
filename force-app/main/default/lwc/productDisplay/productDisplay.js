2e0f75e98f5ee294e22f9b57011f3bbab5eb212a
Gabriel St Jean <gabstjea@publicisgroupe.net>
2023-02-28 09:09:10 -0500

    Facet display updates when pillbox is removed

--- force-app/main/default/lwc/productDisplay/productDisplay.js
+++ force-app/main/default/lwc/productDisplay/productDisplay.js
@@ -1,10 +1,94 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement,track } from 'lwc';
+import communityId from '@salesforce/community/Id';
+import getSearchResults from '@salesforce/apex/ProductPageController.getSearchResults';
+import createFacetDisplay from '@salesforce/apex/ProductPageController.createFacetDisplay';
+// import dummyFacetDisplay from '@salesforce/apex/ProductPageController.dummyFacetDisplay';
+import  filterProductDisplay from '@salesforce/apex/ProductPageController.filterProductDisplay';
+
+// This component serves as a testing ground for the product display and listing component
 
 export default class FilterPrompt extends LightningElement {
-    //@wire(getProductList) productList;
-    @api productList;
-    searchKey;
+   
+    @track productList;
+    @track facetDisplay; // Stores a deep copy of the List<FacetDisplay> object from the createFacetDisplay Apex method 
+    @track activeAccordionSections = [];
+    categoryLandingPage = 'iPhone';
+    clpComboboxOptions =  [
+        {label: "iPhone", value: "iPhone"},
+        {label: "Samsung", value: "Samsung"},
+      ];
+    
+   @track facetPillBox = [];
 
+   imperativeApex() {
+         getSearchResults({communityId: communityId, categoryLandingPage: this.categoryLandingPage})
+            .then(r => {
+              this.productList = r.productsPage.products;
+              console.log(JSON.parse(JSON.stringify(this.productList)));
+              this.facetPillBox = [];
+            })
+            .catch(e => {
+              console.log(e);
+            });
+
+         createFacetDisplay({communityId: communityId, categoryLandingPage: this.categoryLandingPage})
+                .then(r => {
+                    // Create a deep copy of the server response 
+                    this.facetDisplay = JSON.parse(JSON.stringify(r));
+                    console.log(JSON.parse(JSON.stringify(this.facetDisplay)));
+                    // Expand the accordian section whose names appear in the array
+                    this.activeAccordionSections = [];
+                    for (let i = 0; i < this.facetDisplay.length; i++) {
+                      this.activeAccordionSections[i] = this.facetDisplay[i].facetName;
+                    }
+                })
+    } // imperativeApex 
+
+    handleCheckBoxGroup(event) {
+      let selectedFacetDisplay = this.facetDisplay.filter(item => item.facetName === event.target.label)[0];
+      // If the new list of values from the facet display is less than before it was changed, remove the item from the pillbox
+      if (event.detail.value.length < selectedFacetDisplay.selectedFacets.length ) { 
+          let uncheckedItem = selectedFacetDisplay.selectedFacets.filter(e => event.detail.value.includes(e) !== true)[0];      
+          this.facetPillBox = this.facetPillBox.filter( e => e.label.substring(e.label.indexOf(":") + 1).trim() !== uncheckedItem);
+      } else { // add item to pillbox
+          // Since elements are automatically sorted when added into the array, the newly added element can't be retireved from the last index
+          let newlyAddedItem = event.detail.value.filter(e => selectedFacetDisplay.selectedFacets.includes(e) !== true)[0];     
+          this.facetPillBox.push({label: event.target.label + ": " + newlyAddedItem, name: event.target.label});
+      }
+      console.log(event.detail.value);
+      selectedFacetDisplay.selectedFacets = event.detail.value;
+      filterProductDisplay({communityId: communityId, categoryLandingPage: this.categoryLandingPage,  
+                            facetDisplayJson: JSON.stringify(this.facetDisplay)})
+              .then(r => {
+                console.log(r);
+                this.productList = r;      
+              })
+              .catch(e => {
+               console.log(e);
+              });
+    } // handleCheckBoxGroup
+
+    // Updates the facet display when a pill box is x'ed out
+    handleItemRemove(event) {
+        const index = event.detail.index;
+        let pillBoxLabel = event.detail.item.label
+        let facetToUncheck =  pillBoxLabel.substring(pillBoxLabel.indexOf(":") + 1).trim();    
+        // Retrieve the Facet Display item to update
+        let selectedFacetDisplay = this.facetDisplay.filter(item => item.facetName === event.detail.item.name)[0];
+        
+        selectedFacetDisplay.selectedFacets =  selectedFacetDisplay.selectedFacets.filter(e => e !== facetToUncheck );
+        this.facetPillBox.splice(index, 1); // Remove the pillbox  
+        filterProductDisplay({communityId: communityId, categoryLandingPage: this.categoryLandingPage, 
+                            facetDisplayJson: JSON.stringify(this.facetDisplay)})
+              .then(r => {
+                console.log(r);
+                this.productList = r;      
+              })
+              .catch(e => {
+                console.log(e);
+              });
+    }
+    
     connectedCallback() {
         console.log(this.productList);
         console.log(this.productList);
